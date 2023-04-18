8614506a179399b2a0f39a6be7df1c095ef97716
Kirthan <kirthan.cs21@bmsce.ac.in>
2023-04-18 18:25:27 +0530

    project done

---
+++ testing.py
@@ -0,0 +1,3 @@
+print(int(input())+int(input()))
+# go to dist/testing open powershell here write '.\testing.exe' and u will be able to use it. 
+# shift + right click and choose open with powershell --> short cut to open powershell
